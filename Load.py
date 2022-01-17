027e2de616cc46dd62435dbf9e1c62de3a4a3b32
hachemsfar <35065444+hachemsfar@users.noreply.github.com>
2022-01-17 22:37:51 +0100

    Add files via upload

--- Load.py
+++ Load.py
@@ -2,6 +2,8 @@ import Extract_Transform
 from datetime import datetime
 import pandas as pd
 
+from elasticsearch import helpers, Elasticsearch
+import csv
 def load(new_ads):
     new_ads=Extract_Transform.Extract_and_Transform()
     now = datetime.now()
@@ -20,5 +22,10 @@ def load(new_ads):
     df.to_csv("Data/House/Buy/Haus-kaufen_"+str(new_file_date)+".csv",sep=';',index=False)
     
     
+    es = Elasticsearch()
+    
+    for i in new_ads:
+        helpers.bulk(es, i, index='immoscout-index', doc_type='immoscout_docs')
+        
 if __name__ == '__main__':
     load(new_ads)
--- Pipeline.py
+++ Pipeline.py
@@ -2,9 +2,13 @@ from airflow.models import DAG
 from datetime import datetime
 from airflow.operators.python import PythonOperator
 from airflow.operators.dummy_operator import DummyOperator
+
+
+
+
 from textwrap import dedent
 
-import Extract,Transform,Load
+import Extract_Transform,Transform,Load
 
 
 def custom_success_function(context):
@@ -16,7 +20,7 @@ def custom_success_function(context):
 default_arguments = {
     'owner': 'Hachem SFAR',
     'email': 'hachem.sfar@supcom.tn'
-    ,'start_date': datetime(2021, 12, 19)
+    ,'start_date': datetime(2022, 1, 17)
     ,	'email': ['hachem.sfar@supcom.tn']
     ,'email_on_failure': True
     }
@@ -26,42 +30,32 @@ etl_dag = DAG( 'etl_workflow', default_args=default_arguments )
 
 
 
-with DAG('TLC Trip Pipeline',description='TLC Trip',schedule_interval="@monthly",start_date=datetime(2021, 12, 1),catchup=False,tags=['example'],) as dag:
+with DAG('Immobilienscout24 Pipeline',description='Immobilienscout24',schedule_interval="@daily",start_date=datetime(2022, 1, 17),catchup=False) as dag:
     # [END instantiate_dag]
     # [START documentation]
     dag.doc_md = __doc__
     # [END documentation]
-
-
+    
+    
     # [START main_flow]
-    extract_task = PythonOperator(
-        task_id='extract',
-        python_callable=Extract.Extract(),
+    ExtractTransform_task = PythonOperator(
+        task_id='extract_Transform',
+        python_callable=Extract_Transform.Extract_and_Transform(),
     )
     extract_task.doc_md = dedent(
         """\
-    #### Extract task
-    A simple Extract task to get data ready for the rest of the data pipeline.
-    In this case, getting data is simulated by reading from a hardcoded JSON string.
-    This data is then put into xcom, so that it can be processed by the next task.
+    #### Extract/Transform task
+    The Extract task is to get data ready for the rest of the data pipeline.
+    In this case, getting data is simulated by scraping data from immobilienscout24 website
+    
+    The Transform task is to parse the json response and collected the features into dictionnary file
     """
     )
-
-    transform_task = PythonOperator(
-        task_id='transform',
-        python_callable=Transform.Transform(),
-    )
-    transform_task.doc_md = dedent(
-        """\
-    #### Transform task
-    A simple Transform task which takes in the collection of order data from xcom
-    and computes the total order value.
-    This computed value is then put into xcom, so that it can be processed by the next task.
-    """
-    )
-
+    
+    
+    
     load_task = PythonOperator(
-        task_id='load',
+        task_id='Load',
         python_callable=Load.load()
     )
     load_task.doc_md = dedent(
@@ -73,15 +67,15 @@ with DAG('TLC Trip Pipeline',description='TLC Trip',schedule_interval="@monthly"
     )
         
     success_task = DummyOperator(
-  		task_id='success_task',
-  		on_success_callback=custom_success_function
+      		task_id='success_task',
+      		on_success_callback=custom_success_function
       )
     
     success_task.doc_md = dedent(
         """\
     #### success task
-    A simple sucess task which send an email, when the pipeline worked fine.
+    A simple sucess task which send an email, when the pipeline doesn't work fine
     """
     )    
     
-    extract_task >> transform_task >> load_task >> success_task
+    ExtractTransform_task >> load_task >> success_task
